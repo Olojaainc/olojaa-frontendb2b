f66a10f55e5f2d601fd705a549b994b011435a66
toluamole <amoletoluwanimi@gmail.com>
2025-04-16 13:21:30 +0100

    temporarily disable middleware

--- middleware.ts
+++ middleware.ts
@@ -6,27 +6,27 @@ const publicRoutes = ['/signin', '/signup', '/']
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname
-  const isProtectedRoute = protectedRoutes.includes(path)
-  const isPublicRoute = publicRoutes.includes(path)
+  // const isProtectedRoute = protectedRoutes.includes(path)
+  // const isPublicRoute = publicRoutes.includes(path)
 
-  const cookie = req.cookies.get("session")?.value
-  const session = await decrypt(cookie)
+  // const cookie = req.cookies.get("session")?.value
+  // const session = await decrypt(cookie)
 
-  if (isProtectedRoute && (!session?.exp || !session?.userId)) {
-    const redirectResponse = NextResponse.redirect(new URL('/signin', req.nextUrl));
-    redirectResponse.headers.set("x-middleware-cache", "no-cache");
-    return redirectResponse;
-  }
+  // if (isProtectedRoute && (!session?.exp || !session?.userId)) {
+  //   const redirectResponse = NextResponse.redirect(new URL('/signin', req.nextUrl));
+  //   redirectResponse.headers.set("x-middleware-cache", "no-cache");
+  //   return redirectResponse;
+  // }
 
-  if (isPublicRoute && session?.exp && session?.userId) {
-    const redirectResponse = NextResponse.redirect(new URL('/dashboard', req.nextUrl));
-    redirectResponse.headers.set("x-middleware-cache", "no-cache");
-    return redirectResponse;
-  }
+  // if (isPublicRoute && session?.exp && session?.userId ) {
+  //   const redirectResponse = NextResponse.redirect(new URL('/dashboard', req.nextUrl));
+  //   redirectResponse.headers.set("x-middleware-cache", "no-cache");
+  //   return redirectResponse;
+  // }
 
-  return NextResponse.next()
+  // return NextResponse.next()
 }
 
-export const config = {
-  matcher: ['/dashboard/:path*', '/orders/:path*', '/signin', '/signup', '/'],
-}
+// export const config = {
+//   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+// }
