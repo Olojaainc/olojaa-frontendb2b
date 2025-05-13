5ef4f600d038b7efa5c59f4cb17091afce5ab2fd
toluamole <amoletoluwanimi@gmail.com>
2025-05-13 01:59:31 +0100

    Extract columnsDetails to a component file

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -25,42 +25,8 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { columnsDetails } from "../components/ColumnsDetails";
 
-
-export const columnsDetails: ColumnDef<Order>[] = [
-			
-	{
-	  accessorKey: "order_number",
-	  header: "Order ID",
-	},
-	{
-		accessorKey: "quantity",
-		header:() => <div className="text-right">Quantity</div>,
-		cell: ({ row }) => (
-			<div className="text-right">
-				{row.getValue("quantity")}kg
-			</div>
-		)
-	},
-	{
-		accessorKey: "gas_price",
-		header: () => <div className="text-right">Price Per KG</div>,
-		cell: ({ row }) => (
-			<div className="text-right">
-				#{row.getValue("gas_price")}
-			</div>
-		),
-	},
-	{
-		accessorKey: "total_amount",
-		header: () => <div className="text-right">Total Amount</div>,
-		cell: ({ row }) => (
-			<div className="text-right">
-				#{row.getValue("total_amount")}
-			</div>
-		),
-	}
-]
 
 
 
